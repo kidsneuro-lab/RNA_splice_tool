20156341e54704c160a19346ce370ccb18c157ba
Rhett Marchant <rmar4592@uni.sydney.edu.au>
2022-11-09 15:43:48 +1100

    Added subset function
    Added subset function which generates only the gene input for the actual subset function. May add full functionality in future.

--- R/subset.R
+++ R/subset.R
@@ -1,21 +1,48 @@
-#' Split a string
+#' Extract gene coordinates for RNA-seq subsetting
 #'
-#' @param string A character vector with, at most, one element.
-#' @inheritParams stringr::str_split
+#' Returns the gene coordinates for a given entrez_gene_symbol or
+#' RefSeq/Ensembl transcript to be used for RNA-seq subsetting
+#'
+#' @param genes A character vector with the entrez_gene_symbols,
+#' RefSeq transcript ids, or Ensembl gene or transcript ids for the genes for
+#' analysis.
+#' @param hg Either 19 or 38
+#' @param overhang Number of nucleotides flanking the gene to be included
+#' (default: 1000nt)
 #'
 #' @return A character vector.
 #' @export
 #'
 #' @examples
-#' x <- "alfa,bravo,charlie,delta"
-#' str_split_one(x, pattern = ",")
-#' str_split_one(x, pattern = ",", n = 2)
-#'
-#' y <- "192.168.0.1"
-#' str_split_one(y, pattern = stringr::fixed("."))
-#'
-# INPUT = Vector of gene names and/or transcripts
-# OUTPUT = Subsetted Refseq table and GRanges object
+#' == COMING SOON==
 #'
 
-subsetBamfiles <- function(){}
+subsetBamfiles <- function(genes, hg, overhang = 1000){
+
+    if (hg == 38) {
+        Refseq_Genes <- refseq_introns_exons_hg38
+        Ensembl_Genes <- ensembl_allgenes_chr1_Y_hg38
+    } else if (hg == 19) {
+        Refseq_Genes <- refseq_introns_exons_hg19
+        Ensembl_Genes <- ensembl_allgenes_chr1_Y_hg19
+    }
+
+    genes_tx <- data.table("gene_name" = character(), "tx" = character())
+
+    for (gene in seq(1, length(genes))) {
+        if (length(grep("NM_[0-9]+\\.[0-9]+", genes[gene])) > 0) {
+            tx <- stringr::str_extract_all(genes[gene], "NM_[0-9]+\\.[0-9]+")[[1]][1]
+            gene_name <- unique(Refseq_Genes[tx_version_id == tx & region_type == c("intron"), "gene_name"])[[1]]
+        } else {
+            gene_name <- genes[gene]
+            tx <- unique(Refseq_Genes[gene_name == genes[gene] & canonical == 1, "tx_version_id"])[[1]]
+        }
+        genes_tx <- rbind(genes_tx, data.table("gene_name" = gene_name, "tx" = tx))
+    }
+
+    subsetgenes <- Ensembl_Genes[`Gene name` %in% genes_tx$gene_name]
+    forsubset <- paste0("\'","\'","chr",subsetgenes$`Chromosome/scaffold name`,":",subsetgenes$`Gene start (bp)`-overhang,"-",
+                           subsetgenes$`Gene end (bp)`+overhang,"\'","\'")
+    forsubset <- paste(forsubset,collapse=" ")
+    return(forsubset)
+}
