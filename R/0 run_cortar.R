9101f449d9552a16c5dd77b268a44199e45b7fc2
Rhett Marchant <rmar4592@uni.sydney.edu.au>
2022-11-14 14:30:37 +1100

    Added batch capabilites
    Added cortar_batch(), subsetBamfiles() and updated cortar documentation

--- R/0 run_cortar.R
+++ R/0 run_cortar.R
@@ -16,76 +16,81 @@
 #'     for forward stranded or `2` for reverse stranded
 #' @param subset Does the RNA-Seq need to be subsetted to the genes
 #'     of interest? `TRUE`/`FALSE` (Optional, but improves speed of subsequent
-#'     runs.)
+#'     runs; not currently implemented in v1)
 #' @param output_dir A directory path, pointing to the desired location for
 #'     export of cortar results (e.g. `"output/"`)
 #' @param genelist A character vector with genes/RefSeq transcripts of interest
 #'     (Only for panel or research modes; default = NULL)
-#' @param testsamples A character vector with sampleIDs of test samples for
-#'     gene panel testing (Only for panel mode; default = NULL)
 #'
 #' @export
 #'
 #' @examples
 #' #### == COMING SOON == ####
 #'
-
 cortar <- function(file,
                    mode = "default",
                    assembly = "hg38",
                    annotation = "UCSC",
                    paired = TRUE,
                    stranded = 2,
-                   subset = FALSE,
-                   output_dir = "output/test",
-                   genelist = NULL,
-                   testsamples = NULL) {
-
- #Error catching
-  if(!file.exists(file)){
-    stop("File '", file, "' does not exist or is non-readable. getwd()=='",
-         getwd(), "'")
+                   subset = NULL,
+                   output_dir = "~",
+                   genelist = NULL) {
+  # Error catching
+  # file
+  if (!file.exists(file)) {
+    stop(
+      "File '", file, "' does not exist or is non-readable. getwd()=='",
+      getwd(), "'"
+    )
   }
 
-  if(!dir.exists(output_dir)){
-    message("Directory '", output_dir, "' does not exist.
-    Would you like to create this directory?
-    \t 1. Yes
-    \t 2. No"
-            )
-    selection <- readline(prompt="Selection: ")
-    if(selection == "1"){
-      dir.create(output_dir)
-      message("Directory '", output_dir, "' created.")
-      message("")
-    }else{
-      stop("Directory '", output_dir, "' does not exist. getwd()=='",
-         getwd(), "'")
+  # mode
+  if (mode != "default") {
+    if (mode == "panel" | mode == "research") {
+    } else {
+      stop("'", mode, "' is not an available cortar mode ('default','panel','research')")
     }
   }
 
-  if(mode != "default"){
-        if(mode == "panel" | mode == "research"){
-        }else{
-            stop("'",mode,"' is not an available cortar mode ('default','panel','research')")
-        }
-  }
-
-  if(assembly %nin% c("hg19","hg38")){
+  # assembly
+  if (assembly %nin% c("hg19", "hg38")) {
     stop("Assembly '", assembly, "' is not an available assembly ('hg38','hg19')")
   }
 
-  if(annotation %nin% c("1000genomes","UCSC")){
+  # annotation
+  if (annotation %nin% c("1000genomes", "UCSC")) {
     stop("Annotation '", annotation, "' is not an available annotation ('1000genomes','UCSC')")
   }
 
-  if(class(paired) != "logical"){
+  # paired
+  if (class(paired) != "logical") {
     stop("Paired must be a logical: TRUE, FALSE.
-         Supplied:", paired,"
+         Supplied:", paired, "
          Note: You may need to remove quotation marks.")
   }
 
+  # output_dir
+  if (!dir.exists(output_dir)) {
+    message("Directory '", output_dir, "' does not exist.
+    Would you like to create this directory?
+    \t 1. Yes
+    \t 2. No")
+    selection <- readline(prompt = "Selection: ")
+    if (selection == "1") {
+      dir.create(output_dir)
+      message("Directory '", output_dir, "' created.")
+      message("")
+    } else {
+      stop(
+        "Directory '", output_dir, "' does not exist. getwd()=='",
+        getwd(), "'"
+      )
+    }
+  }
 
+
+  # Initialisation messages
   message(paste0("Running cortar "))
   message(paste0("        file: ", file))
   message(paste0("        mode: ", mode))
@@ -96,29 +101,29 @@ cortar <- function(file,
   message(paste0("      output: ", output_dir))
   message("")
 
-
+  # Read in cortar samplefile
   file <- data.table::fread(file)
 
-  if(mode == "panel" | mode == "research"){
-      genes_tx <- selectGenesTranscripts(
-        genes = genelist,
-        assembly = assembly,
-        annotation = annotation
-      )
-    }
-
-  if(mode == "default"){
-      genes_tx <- selectGenesTranscripts(
-        genes = file$genes,
-        assembly = assembly,
-        annotation = annotation
-      )
-    }
-
-  if (subset == T) {
-    subsetBamfiles
+  # Select genes and transcripts of interest
+  # A genelist must be provided for panel or research mode
+  if (mode == "panel" | mode == "research") {
+    genes_tx <- selectGenesTranscripts(
+      genes = genelist,
+      assembly = assembly,
+      annotation = annotation
+    )
   }
 
+  # Genes specified in the cortar samplefile are used in default
+  if (mode == "default") {
+    genes_tx <- selectGenesTranscripts(
+      genes = file$genes,
+      assembly = assembly,
+      annotation = annotation
+    )
+  }
+
+  # Reads for the genes and transcripts of interest are extracted and counted
   reads <- extractCountReads(
     genes.GRanges = genes_tx[[1]],
     introns.GRanges = genes_tx[[2]][[1]],
@@ -131,6 +136,7 @@ cortar <- function(file,
     stranded = 2
   )
 
+  # Events supported by extracted reads are annotated and quantified
   events <- annotateQuantifyEvents(
     ids = file$sampleID,
     combined_sj = reads,
@@ -140,12 +146,14 @@ cortar <- function(file,
     assembly = assembly
   )
 
+  # Comparisons of events between test samples and controls are performed
   comparisons <- compareSplicing(
     all_splicing_events = events,
     Sample_File = file,
     mode = mode
   )
 
+  # Reports in excel and pdf formats are generated for comparisons
   generateReport(
     comparisons = comparisons,
     Sample_File = file,
@@ -153,8 +161,101 @@ cortar <- function(file,
     mode = mode
   )
 
-
+  # Termination message
   message("")
   message(paste("Done! Reports saved in:", output_dir))
+}
 
+#' Run multiple instances of cortar
+#'
+#' `cortar_batch()` runs the entire cortar pipeline with specified parameters,
+#' and returns excel & pdf reports for multiple samplefiles.
+#'
+#' @param folder A character vector of the full file path, pointing to a folder
+#'     containing one or more cortar samplefiles
+#' @param pattern An optional regular expression. Only file names which match
+#'     the regular expression will be returned (default: `"*.tsv"`).
+#' @inheritParams cortar
+#'
+#' @export
+#'
+#' @examples
+#' #### == COMING SOON == ####
+#'
+cortar_batch <- function(folder,
+                           pattern = "*.tsv",
+                           mode = "default",
+                           assembly = "hg38",
+                           annotation = "UCSC",
+                           paired = T,
+                           stranded = 2,
+                           subset = F,
+                           output_dir = "~",
+                           genelist = NULL){
+  batches_in <- sapply(list.files(folder, pattern = pattern),function(x){paste0(folder,"/",x)})
+  batches_out <- sapply(list.files(folder, pattern = pattern),function(x){paste0(output_dir,"/",strsplit(x,"\\.")[[1]][1])})
+  for(batch in seq(1,length(batches_in))){
+    if(!dir.exists(batches_out[batch])){
+      message("'",batches_out[batch],"' created.")
+      dir.create(batches_out[batch])
+    }
+    cortar(file = batches_in[batch],
+           mode = mode,
+           assembly = assembly,
+           annotation = annotation,
+           paired = paired,
+           stranded = stranded,
+           subset = subset,
+           output_dir = batches_out[batch],
+           genelist = genelist)
+  }
+}
+
+#' Extract gene coordinates for RNA-seq subsetting
+#'
+#' Returns the gene coordinates for a given entrez_gene_symbol or
+#' RefSeq/Ensembl transcript to be used for RNA-seq subsetting
+#'
+#' @param genes A character vector with the entrez_gene_symbols,
+#' RefSeq transcript ids, or Ensembl gene or transcript ids for the genes for
+#' analysis.
+#' @param hg Either 19 or 38
+#' @param overhang Number of nucleotides flanking the gene to be included
+#' (default: 1000nt)
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' == COMING SOON==
+#'
+
+subsetBamfiles <- function(genes, hg, overhang = 1000){
+
+  if (hg == 38) {
+    Refseq_Genes <- refseq_introns_exons_hg38
+    Ensembl_Genes <- ensembl_allgenes_chr1_Y_hg38
+  } else if (hg == 19) {
+    Refseq_Genes <- refseq_introns_exons_hg19
+    Ensembl_Genes <- ensembl_allgenes_chr1_Y_hg19
+  }
+
+  genes_tx <- data.table("gene_name" = character(), "tx" = character())
+
+  for (gene in seq(1, length(genes))) {
+    if (length(grep("NM_[0-9]+\\.[0-9]+", genes[gene])) > 0) {
+      tx <- stringr::str_extract_all(genes[gene], "NM_[0-9]+\\.[0-9]+")[[1]][1]
+      gene_name <- unique(Refseq_Genes[tx_version_id == tx & region_type == c("intron"), "gene_name"])[[1]]
+    } else {
+      gene_name <- genes[gene]
+      tx <- unique(Refseq_Genes[gene_name == genes[gene] & canonical == 1, "tx_version_id"])[[1]]
+    }
+    genes_tx <- rbind(genes_tx, data.table("gene_name" = gene_name, "tx" = tx))
+  }
+
+  subsetgenes <- Ensembl_Genes[`Gene name` %in% genes_tx$gene_name]
+  forsubset <- paste0("\'","\'","chr",subsetgenes$`Chromosome/scaffold name`,":",subsetgenes$`Gene start (bp)`-overhang,"-",
+                      subsetgenes$`Gene end (bp)`+overhang,"\'","\'")
+  forsubset <- paste(forsubset,collapse=" ")
+  return(forsubset)
 }
