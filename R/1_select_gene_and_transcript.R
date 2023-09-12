78ee726bb9dc55e2b78a01e1e8401a20cef40303
Rhett Marchant <rmar4592@uni.sydney.edu.au>
2023-09-12 14:09:55 +1000

    Added testing for tx_extraction()

--- R/1_select_gene_and_transcript.R
+++ R/1_select_gene_and_transcript.R
@@ -76,10 +76,21 @@ tx_extraction <- function(genes,
   for (gene in seq(1, length(genes))) {
     if (length(grep("NM_[0-9]+\\.[0-9]+", genes[gene])) > 0) {
       tx <- stringr::str_extract_all(genes[gene], "NM_[0-9]+\\.[0-9]+")[[1]][1]
-      gene_name <- unique(refseq_assembly[tx_version_id == tx & region_type == c("intron"), "gene_name"])[[1]]
-    } else {
+      if(tx %in% refseq_assembly[,tx_version_id]){
+        gene_name <- unique(refseq_assembly[tx_version_id == tx & region_type == c("intron"), "gene_name"])[[1]]
+      } else {
+        stop(paste0("Transcript identifier `",tx,"` is invalid"))
+      }
+    } else if (length(grep("[A-Za-z0-9]{3,10}", genes[gene])) > 0){
       gene_name <- genes[gene]
-      tx <- unique(refseq_assembly[gene_name == genes[gene] & canonical == 1, "tx_version_id"])[[1]]
+      if(genes[gene] %in% refseq_assembly[,gene_name]){
+        tx <- unique(refseq_assembly[gene_name == genes[gene] & canonical == 1, "tx_version_id"])[[1]]
+      } else {
+        stop(paste0("Gene name `",genes[gene],"` is invalid"))
+      }
+    } else {
+      gene_name <- NULL
+      tx <- NULL
     }
     genes_tx <- rbind(genes_tx, data.table("gene_name" = gene_name, "tx" = tx))
   }
--- tests/testthat/test-1_select_gene_and_transcript.R
+++ tests/testthat/test-1_select_gene_and_transcript.R
@@ -1,3 +1,64 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(testthat)
+
+# TX_EXTRACTION ---------------------------------------------------------------
+
+# Mock assembly data based on provided refseq_introns_exons_hg38 data
+load(testthat::test_path("refseq_intron_exons_hg38_mock.rda"))
+refseq_mock <- refseq_intron_exons_hg38_mock
+
+# Mock gene data
+mock_genes <- c("EMD", "COL2A1")
+
+# 1. Basic functionality
+test_that("tx_extraction correctly parses gene names", {
+  result <- tx_extraction(mock_genes, refseq_mock)
+  expected_output <- data.table::data.table(
+    gene_name = c("EMD", "COL2A1"),
+    tx = c("NM_000117.3", "NM_001844.5")
+  )
+  expect_equal(result, expected_output)
 })
+
+# 2. Mismatched or invalid input
+test_that("tx_extraction throws an error for invalid gene names", {
+  expect_error(tx_extraction(c("NON_EXISTENT"), refseq_mock),
+               "Gene name `NON_EXISTENT` is invalid")
+})
+
+test_that("tx_extraction throws an error for invalid transcripts", {
+  expect_error(tx_extraction(c("NM_001844.0"), refseq_mock),
+               "Transcript identifier `NM_001844.0` is invalid")
+})
+
+# 3. Mix of gene names, transcripts, and blank
+test_that("tx_extraction can handle mixed input", {
+  mixed_input <- c("EMD", "NM_033150.3", "")
+  result <- tx_extraction(mixed_input, refseq_mock)
+  expected_output <- data.table::data.table(
+    gene_name = c("EMD", "COL2A1"),
+    tx = c("NM_000117.3", "NM_033150.3")
+  )
+  expect_equal(result, expected_output)
+})
+
+# 4. Edge cases
+test_that("tx_extraction returns empty data.table for empty input", {
+  result <- tx_extraction(character(0), refseq_mock)
+  expect_equal(nrow(result), 0)
+})
+
+test_that("tx_extraction returns empty data.table for blank input", {
+  result <- tx_extraction("", refseq_mock)
+  expect_equal(nrow(result), 0)
+})
+
+# 5. Assembly Data (assuming refseq_introns_exons_hg38 has a similar structure to the mock data)
+# Note: Modify this test to suit the actual data structure of refseq_introns_exons_hg38
+test_that("tx_extraction handles different assemblies", {
+  result <- tx_extraction(mock_genes, refseq_introns_exons_hg38)
+  # Modify the expectation based on the expected behavior for this scenario
+  expect_true(nrow(result) > 0)
+})
+
+
+# GENE_TO_GRANGE --------------------------------------------------------------
